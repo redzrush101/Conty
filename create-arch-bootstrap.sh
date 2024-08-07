eee068151e31cd49281a2a014057e637ca30e70f
redzrush101 <yassin@github.com>
2024-08-07 12:01:58 +0200

    Revert "remove bloat"
    This reverts commit c0d25d1b7e90b6ed2a6fcca9956b113b844d9e67.

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -16,14 +16,36 @@ video_pkgs="mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
 	libva-intel-driver lib32-libva-intel-driver intel-media-driver \
 	mesa-utils vulkan-tools libva-utils lib32-mesa-utils"
 
+wine_pkgs="wine-staging winetricks-git wine-nine wineasio \
+	giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap \
+	gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal \
+	v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins \
+	lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo \
+	lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama \
+	lib32-libxinerama libxslt lib32-libxslt libva lib32-libva gtk3 \
+	lib32-gtk3 vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2 \
+	vkd3d lib32-vkd3d libgphoto2 ffmpeg gst-plugins-good gst-plugins-bad \
+	gst-plugins-ugly gst-plugins-base lib32-gst-plugins-good \
+	lib32-gst-plugins-base gst-libav wget gst-plugin-pipewire"
+
 devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
 
 # Packages to install
 # You can add packages that you want and remove packages that you don't need
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
-export packagelist="${audio_pkgs} ${video_pkgs} ${devel_pkgs} \
-	nano ttf-dejavu ttf-liberation"
+export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
+	nano ttf-dejavu ttf-liberation lutris steam firefox mpv geany pcmanfm \
+	htop qbittorrent speedcrunch gpicview file-roller xorg-xwayland \
+	steam-native-runtime gamemode lib32-gamemode jre17-openjdk lxterminal \
+	steamtinkerlaunch mangohud lib32-mangohud qt6-wayland wayland \
+	lib32-wayland qt5-wayland retroarch xorg-server-xephyr openbox \
+	obs-studio gamehub minigalaxy legendary gamescope prismlauncher yt-dlp \
+	bottles playonlinux minizip retroarch-assets-ozone libretro-beetle-psx-hw \
+	libretro-blastem libretro-bsnes libretro-dolphin libretro-duckstation \
+	libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia \
+	libretro-parallel-n64 libretro-pcsx2 libretro-picodrive libretro-ppsspp \
+	libretro-retrodream libretro-yabause sunshine"
 
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist=""
@@ -36,12 +58,12 @@ export aur_packagelist=""
 # Arch Linux repos will be replaced with their optimized versions from ALHP
 #
 # Set this variable to true, if you want to enable this repository
-enable_alhp_repo="true"
+enable_alhp_repo="false"
 
 # Feature levels for ALHP. Available feature levels are 2 and 3
 # For level 2 you need a CPU with SSE4.2 instructions
 # For level 3 you need a CPU with AVX2 instructions
-alhp_feature_level="3"
+alhp_feature_level="2"
 
 ########################################################################
 
